927fc74a5db5e35d4d0ba8b738ed5bd37c308d6e
mukizaemma <emmatech64@gmail.com>
2025-08-11 22:26:28 +0200

    some changes

--- resources/views/admin/activities.blade.php
+++ resources/views/admin/activities.blade.php
@@ -19,7 +19,7 @@
             <div class="container-fluid px-4">
                 {{-- <h1 class="mt-4">Dashboard</h1> --}}
                 <ol class="breadcrumb mb-4">
-                    <li class="breadcrumb-item active">Impact Life Mission Missions Projects</li>
+                    <li class="breadcrumb-item active">Impact Life Mission Projects</li>
                 </ol>
                 <div class="row">
                     @if(session()->has('success'))
--- resources/views/layouts/adminbase.blade.php
+++ resources/views/layouts/adminbase.blade.php
@@ -10,7 +10,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Impact Life Mission - Dashboard {{$data->company ?? ''}}</title>
+        <title>Impact Life Mission - Dashboard</title>
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
         <link href="{{asset('assets')}}/admin/css/styles.css" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
@@ -29,7 +29,7 @@
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3" href="index.html">Impact Life Mission Missions</a>
+            <a class="navbar-brand ps-3" href="index.html">Impact Life Mission</a>
             <!-- Sidebar Toggle-->
             <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
             <!-- Navbar Search-->
