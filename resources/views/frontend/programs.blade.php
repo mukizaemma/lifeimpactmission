9782a23e70ec6c1b35133ce43e77d72922a81fca
mukizaemma <emmatech64@gmail.com>
2025-08-11 20:17:21 +0200

    some changes

--- resources/views/frontend/programs.blade.php
+++ resources/views/frontend/programs.blade.php
@@ -51,7 +51,7 @@
                                 $words = Str::limit($rs->description, 100, '...');
                                 @endphp
 
-                                <p style="font-size: 20px; font-wight:700; text-align:left" > {{ $words }} </p>
+                                <p style="font-size: 20px; font-wight:700; text-align:center" > {{ $words }} </p>
 
                                 @if(strlen($rs->description) > 100)
                                 <a href="{{route('singleProgram',['slug'=>$rs->slug])}}">
