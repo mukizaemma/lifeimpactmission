0e0d08badb3d6b9bf4bdb7d6aa78f539afe017ac
mukizaemma <emmatech64@gmail.com>
2025-08-11 19:47:41 +0200

    some changes

--- resources/views/frontend/includes/slides.blade.php
+++ resources/views/frontend/includes/slides.blade.php
@@ -1,36 +1,42 @@
-        <div class="tp-slider-3__area p-relative">
-            <div class="tp-slider-3__icon smooth">
-                <a href="#service" class="arrow-scroll-btn"><i class="flaticon-down-arrow"></i></a>
-            </div>
+<div class="tp-slider-3__area p-relative">
+    <div class="tp-slider-3__icon smooth">
+        <a href="#service" class="arrow-scroll-btn"><i class="flaticon-down-arrow"></i></a>
+    </div>
 
-            <div class="tp-slider-3__wrapper">
-                <div class="swiper-container tp-slider-3__active">
-                    <div class="swiper-wrapper">
-                        @foreach ($slides as $slide)
-                        <div class="swiper-slide">
-                            <div class="tp-slider-3__bg z-index fix p-relative" data-background="{{asset('storage/images/slides/'). $slide->image}}">
-                                <div class="tp-slider-3__shape">
-                                    <img src="assets/img/slider/slider-shape-3-1.png" alt="">
-                                </div>
-                                <div class="tp-slider-3__text">
-                                    <h3 class="tp-slider-3__big-text"></h3>
-                                </div>
-                                <div class="container">
-                                    <div class="row">
-                                        <div class="col-xl-12">
-                                            <div class="tp-slider-3__content text-center">
-                                                <span class="tp-slider-3-subtitle">Impact Life Mission</span>
-                                                <h2 class="tp-slider-3-title pb-30">{{ $slide->heading }}</h2>
-                                                <a class="tp-btn theme-1-bg" href="{{ route('backgroundDetails') }}">Discover More</a>
-                                            </div>
+
+    <!-- Slider Wrapper (80% width and centered) -->
+    <div class="tp-slider-3__wrapper" style="width: 80%; margin: 0 auto;">
+        <div class="swiper-container tp-slider-3__active">
+            <div class="swiper-wrapper">
+                @foreach ($slides as $slide)
+                    <div class="swiper-slide">
+                        <div class="tp-slider-3__bg z-index fix p-relative"
+                             style="background-image: url('{{ asset('storage/images/slides/' . $slide->image) }}'); 
+                                    background-size: contain; 
+                                    background-position: center; 
+                                    background-repeat: no-repeat; 
+                                    height: 700px; 
+                                    border-radius: 10px; 
+                                    overflow: hidden;">
+                        
+
+                            <div class="tp-slider-3__text">
+                                <h3 class="tp-slider-3__big-text"></h3>
+                            </div>
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-xl-12">
+                                        <div class="tp-slider-3__content text-center">
+                                            <h2 class="tp-slider-3-title pb-30">{{ $slide->heading }}</h2>
+                                            <a class="tp-btn theme-2-bg" href="{{ route('backgroundDetails') }}">Get Involved</a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        @endforeach
-
-                    </div>
-                </div>
+                        </div> <!-- tp-slider-3__bg -->
+                    </div> <!-- swiper-slide -->
+                @endforeach
             </div>
         </div>
+    </div>
+</div>
